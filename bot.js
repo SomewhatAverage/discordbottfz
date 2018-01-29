89097d3580597e74d4fbbf0db7c7f2419359293f
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-01-29 17:06:04 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,48 @@
+// cd C:\Users\tfzam\Desktop\greeter-bot
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+const prefix = "!";
+client.on("message", (message) => {
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  var msg = message.content.toUpperCase();
+  // Exit and stop if it's not there
+    if (msg.includes('FUCK') || msg.includes('ASS') || msg.includes('ASSHOLE') || msg.includes('BITCH') || msg.includes('CUNT') || msg.includes('DICK') || msg.includes('MOTHERFUCKER') || msg.includes('NIGGA') || msg.includes('SHIT') || msg.includes('PUSSY') || msg.includes('NIGGER') || msg.includes('F*CK') || msg.includes('SH*T') || msg.includes('F---') || msg.includes('S---')) {
+	message.delete(); 
+  }
+  if (message.content === 'Dell') {
+	  message.channel.bulkDelete(100);
+  }
+  if (!message.content.startsWith(prefix)) return;
+
+  if (command == 'num') {
+    message.channel.send(Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000);
+  } else
+  if (command === "clear") {
+  let x = args[0];
+	message.channel.bulkDelete(x);
+} else
+ if (command === 'lenny') {
+	 message.channel.send("( ͡° ͜ʖ ͡°)");
+ } else
+if (command === 'mute') {
+let role = message.guild.roles.find("name", "Muted");
+let member = message.mentions.members.first();
+member.addRole(role).catch(console.error);
+} else
+  if (command === 'kick') {
+    let modRole = message.guild.roles.find("name", "Mod");
+    if(message.member.roles.has(modRole.id)) { 
+      let kickMember = message.guild.member(message.mentions.users.first());
+      message.guild.member(kickMember).kick();
+      message.channel.sendMessage("Member Kicked.");
+    } else {
+      return message.reply("You don't have the perms to kick members.");
+    }
+  }
+});
+client.login("Mzk1Nzc5NDQzODEwODkzODM0.DU7hNg.a7bMuHQUCCMLmPUE9AGmJBDi8-I");
+// Math.floor(Math.random() * (9999 - 1000)) + 1000
