79df4f7c302dda3364165bd2c73991e63b5295f5
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-01-29 19:07:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ if (command === 'ban') {
 	message.channel.sendMessage("https://giphy.com/gifs/fire-skeleton-funny-B0yHMGZZLbBxS");
    } else
 	if (command === 'alertadmin') {
-		message.channel.sendMessage("**Somebody's Getting Banned Today...** https://www.tenor.co/JZWm.gif @Admin
+		message.channel.sendMessage("**Somebody's Getting Banned Today...** https://www.tenor.co/JZWm.gif @Admin");
 	}
 });
 client.login(process.env.BOT_TOKEN);
