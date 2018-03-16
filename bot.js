6fad4919e0fe49b94c5543df791028ce274d9476
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-03-16 17:26:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
 console.log("I am ready!");
-client.user.setActivity('YouTube', { type: 'WATCHING' })
 });
 const prefix = "!";
+client.user.setActivity('YouTube', { type: 'WATCHING' })
 client.on("message", (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
