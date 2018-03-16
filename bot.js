b86958dcd89f7dad53d46e4083ac86d228da1e23
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-03-15 23:24:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
 console.log("I am ready!");
-client.user.setPresence({ game: { name: 'on the MMS Discord Server!' }, status: 'online' })
+client.user.setActivity('YouTube', { type: 'WATCHING' })
 });
 const prefix = "!";
 client.on("message", (message) => {
