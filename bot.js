8abd5a427f61f8ad0048b0453c2aae1ff0549f30
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-03-15 21:53:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -173,5 +173,8 @@ message.channel.sendMessage(secondValue)
 client.on('guildMemberAdd', member => {
     member.guild.channels.get('410196865775304704').send('**' + member.user.username + '**, has joined the server!'); 
 });
+client.on('guildMemberRemove', member => {
+    member.guild.channels.get('410196865775304704').send('**' + member.user.username + '**, has left the server.'); 
+});
 client.login(process.env.BOT_TOKEN);
 // Math.floor(Math.random() * (9999 - 1000)) + 1000
