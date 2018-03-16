7a9f4f595825a6b7cfe490e6ee232ecc544a2205
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-03-16 17:06:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
 console.log("I am ready!");
-client.user.setActivity('YouTube', { type: 'WATCHING' })
+client.user.setGame(`on ${client.guilds.size} servers`);
 });
 const prefix = "!";
 client.on("message", (message) => {
