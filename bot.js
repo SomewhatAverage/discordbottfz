084c9fff0cf830ae8730682147081ff683a908e5
SomewhatAverage <34795103+SomewhatAverage@users.noreply.github.com>
2018-03-15 23:06:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on("ready", () => {
-client.user.setGame('on the MMS Discord Server!');
+bot.user.setGame('on the MMS Discord Server!');
 console.log("I am ready!");
 });
 const prefix = "!";
